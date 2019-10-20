f73bb1f507f77a18614692493c4afe0b1375cd82
Hasan <swehu1988@gmail.com>
2019-10-20 15:17:18 +1100

    get all answers

--- Assets/Scripts/MultipleChoice.cs
+++ Assets/Scripts/MultipleChoice.cs
@@ -7,8 +7,9 @@ public class MultipleChoice : MonoBehaviour
 {
     public string question;
     public GameObject[] disableBtn;
-
-    public void ButtonEnabler(GameObject answerName)
+    public SaveData _data;
+    public bool isGeneral, isJoint, isMorning, isFatigue, isDay;
+     public void ButtonEnabler(GameObject answerName)
     {
         print(answerName.name);
         foreach (GameObject btn in disableBtn)
@@ -17,5 +18,19 @@ public class MultipleChoice : MonoBehaviour
 
         answerName.GetComponent<Image>().color = new Color(0.4f, 0.4f, 0.4f, 0.4f);
 
+        if (isGeneral)
+            _data.getGeneralFeeling(answerName.name);
+
+        if (isJoint)
+            _data.getMyJoint(answerName.name);
+
+        if (isMorning)
+            _data.getMyMorning(answerName.name);
+
+        if (isFatigue)
+            _data.getMyFatigue(answerName.name);
+
+        if (isDay)
+            _data.getMyDay(answerName.name);
     }
 }
--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -13,28 +13,32 @@ public class SaveData : MonoBehaviour
         indexer = PlayerPrefs.GetInt("dataIndex");
         print(indexer);
 
-        if (PlayerPrefs.GetString("my_feeling_"+indexer) != null &&
-            PlayerPrefs.GetString("my_joint_" + indexer) != null &&
-            PlayerPrefs.GetString("my_morning_" + indexer) != null &&
-            PlayerPrefs.GetString("my_fatigue_" + indexer) != null &&
-            PlayerPrefs.GetString("my_myday_" + indexer) != null
+        if (PlayerPrefs.GetString("my_feeling_" + indexer).Length != 0 &&
+            /*PlayerPrefs.GetString("my_joint_" + indexer).Length != 0 &&*/
+            PlayerPrefs.GetString("my_morning_" + indexer).Length != 0 &&
+            PlayerPrefs.GetString("my_fatigue_" + indexer).Length != 0 &&
+            PlayerPrefs.GetString("my_myday_" + indexer).Length != 0
             )
         {
             PlayerPrefs.SetInt("dataIndex", (indexer + 1));
+            // save to excel:
+
+
+            // send to email
         }
     }
     void Start()
     {
 
         checkIndex();
-        print(DateTime.Now);
     }
 
 
     public void getGeneralFeeling(string userInput)
     {
         checkIndex();
-        PlayerPrefs.SetString("my_feeling" + indexer, userInput);
+        PlayerPrefs.SetString("my_feeling_" + indexer, userInput);
+        PlayerPrefs.SetString("my_feeling_time", DateTime.Now.ToString());
     }
 
     public void getMyJoint(string userInput)
@@ -45,14 +49,23 @@ public class SaveData : MonoBehaviour
     public void getMyMorning(string userInput)
     {
         checkIndex();
+        PlayerPrefs.SetString("my_morning_" + indexer, userInput);
+        PlayerPrefs.SetString("my_morning_time", DateTime.Now.ToString());
+
     }
     public void getMyFatigue(string userInput)
     {
         checkIndex();
+        PlayerPrefs.SetString("my_fatigue_" + indexer, userInput);
+        PlayerPrefs.SetString("my_fatigue_time", DateTime.Now.ToString());
+
     }
     public void getMyDay(string userInput)
     {
         checkIndex();
+        PlayerPrefs.SetString("my_myday_" + indexer, userInput);
+        PlayerPrefs.SetString("my_myday_time", DateTime.Now.ToString());
+
     }
     public void getMyMedication(string userInput)
     {
